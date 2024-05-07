1157050c7bc8db1867ac5c8bac615574187fdd59
unknown <ken1999429@gmail.com>
2024-05-07 23:46:23 +0800

    first commit

---
+++ TestClient.py
@@ -0,0 +1,21 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+#IP = socket.gethostbyname(socket.gethostname())
+IP = "192.168.1.104"
+ADDR = (IP,PORT)
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode('utf-8')
+    msg_length = len(message)
+    send_length = str(msg_length).encode('utf-8')
+    send_length += b' ' * (HEADER-len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("Hello World!")
+    
---
+++ TestServer.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+HEADER = 64
+DISCONNECT_MSG = "!DISCONNTECT"
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname()) #SERVER = "192.168.1.104"
+Addr = (SERVER,PORT)
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(Addr)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode('utf-8')
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode('utf-8')
+        if msg == DISCONNECT_MSG:
+            connected = False
+        print(f'[{addr}] {msg}')
+    conn.close()
+    
+def start():
+    server.listen()
+    print(f'LISTENING on {server}')
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTION] {threading.activeCount() - 1}")
+
+print("[STARTING NOW]")
+start()
+
+
